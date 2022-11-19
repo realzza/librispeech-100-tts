1243f7c68faee05b519c14fda95365bcfd727215
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-11-18 20:36:26 -0500

    adjusted run script to librispeech 100

--- run.sh
+++ run.sh
@@ -5,23 +5,16 @@ set -e
 set -u
 set -o pipefail
 
-fs=22050
+fs=16000
 n_fft=1024
 n_shift=256
 
-opts=
-if [ "${fs}" -eq 22050 ]; then
-    # To suppress recreation, specify wav format
-    opts="--audio_format wav "
-else
-    opts="--audio_format flac "
-fi
 
-train_set=tr_no_dev
-valid_set=dev
-test_sets="dev eval1"
+train_set="train_clean_100"
+valid_set="dev"
+test_sets="test_clean test_other dev_clean dev_other"
 
-train_config=conf/train.yaml
+train_config=conf/tuning/train_transformer.yaml
 inference_config=conf/decode.yaml
 
 # g2p=g2p_en # Include word separator
@@ -42,4 +35,4 @@ g2p=g2p_en_no_space # Include no word separator
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
     --srctexts "data/${train_set}/text" \
-    ${opts} "$@"
+    --audio_format "flac.ark" "$@"
